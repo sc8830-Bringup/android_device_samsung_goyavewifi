3798494e06551f9777d291db3fc17d1039d0328f
Remilia Scarlet <remilia.1505@gmail.com>
2018-06-16 16:16:55 +0000

    goyavewifi: patches: Add required patches for building process
    Change-Id: I515141587c7adbace0bef9506a74e54f289364c7
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,6 +18,9 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
+# Apply patches first
+sh device/samsung/goyavewifi/patches/apply.sh;
+
 for i in eng user userdebug; do
 add_lunch_combo lineage_goyavewifi-${i};
 done
