59ee7f58e24b89bf0e0f8a1755fd1db32bb089fc
ahnet-69 <syedmusicrealise@gmail.com>
2024-12-22 12:19:13 +0000

    goyavewifi: Fixup proprietary-files
    * Correct Bluetooth firmware
    * Point extract script to local dump
    * Remove non existent gps.cer file

--- extract-files.sh
+++ extract-files.sh
@@ -11,7 +11,7 @@ for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
     if [ ! -d $BASE/$DIR ]; then
         mkdir -p $BASE/$DIR
     fi
-    cp ../../../../g531h/stock/$FILE $BASE/$FILE
+    cp ~/roms/goyavewifi/system/$FILE $BASE/$FILE
     echo "$FILE pulled"
 done
 
